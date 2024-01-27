1a716c89d05cba004a5a4548c505c581a7d7f132
shivaprasadnashte <nashted.ug21.ee@nitp.ac.in>
2024-01-27 10:44:18 +0530

    minor fixes

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,5 +1,5 @@
 import SCARD from "../Components/SmallCard";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import left from "../asets/chevronleft.svg";
 import right from "../asets/chevronright.svg";
 
@@ -26,55 +26,58 @@ function Body() {
     //eslint-disable-next-line
   }, [page]);
 
-  
   return (
     <>
-      { loding ? <div className=" flex justify-center items-center h-screen">
-        <div className=" animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-      </div> :
-        
+      {loding ? (
+        <div className=" flex justify-center items-center h-screen">
+          <div className=" animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
+        </div>
+      ) : (
         <main className=" flex flex-col items-center gap-6 pb-6">
-        <p className=" text-2xl font-bold">LATEST NEWS</p>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 justify-evenly">
-          {data.articles?.map((item, index) => {
-            return (
-              <SCARD
-              key={index}
-                title={item.title}
-                description={item.description}
-                img={item.urlToImage || "https://picsum.photos/200/300"}
-                url={item.url}
-              />
-            );
-          })}
-        </div>
-        <div className=" flex">
-          {(page>12)&&<div className="  shadow-xl w-24 items-center rounded-md flex gap-1">
-            <img src={left} alt="#" className=" h-4 text-center" />
-            <button
-              onClick={() => {
-                setPage(page - 12);
-              }}
-            >
-              Previous
-            </button>
-          </div>}
-          <div className=" px-6">
-            <p>{page / 12}</p>
+          <p className=" text-2xl font-bold">LATEST NEWS</p>
+          <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 justify-evenly">
+            {data.articles?.map((item, index) => {
+              return (
+                <SCARD
+                  key={index}
+                  title={item.title}
+                  description={item.description}
+                  img={item.urlToImage || "https://picsum.photos/200/300"}
+                  url={item.url}
+                />
+              );
+            })}
           </div>
+          <div className=" flex">
+            {page > 12 && (
+              <div className="  shadow-xl w-24 items-center rounded-md flex gap-1">
+                <img src={left} alt="#" className=" h-4 text-center" />
+                <button
+                  onClick={() => {
+                    setPage(page - 12);
+                  }}
+                >
+                  Previous
+                </button>
+              </div>
+            )}
+            <div className=" px-6">
+              <p>{page / 12}</p>
+            </div>
 
-          <div className=" shadow-xl w-20 text-center px-2 rounded-md flex">
-            <button
-              onClick={() => {
-                setPage(page + 12);
-              }}
-            >
-              Next
-            </button>
-            <img src={right} alt="#" />
+            <div className=" shadow-xl w-20 text-center px-2 rounded-md flex">
+              <button
+                onClick={() => {
+                  setPage(page + 12);
+                }}
+              >
+                Next
+              </button>
+              <img src={right} alt="#" />
+            </div>
           </div>
-        </div>
-      </main>}
+        </main>
+      )}
     </>
   );
 }
